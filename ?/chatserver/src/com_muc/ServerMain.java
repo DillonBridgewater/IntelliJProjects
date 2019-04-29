e9724276b85079e00b4cc9655d9ad7638bcfab2b
DillonBridgewater <dbrid3564@westada.org>
2019-04-29 12:44:03 -0600

    initial update First functional telnet + putty MultiThreading

--- ?/chatserver/src/com_muc/ServerMain.java
+++ ?/chatserver/src/com_muc/ServerMain.java
@@ -6,6 +6,7 @@ import java.net.DatagramPacket;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Date;
+import java.util.zip.CheckedOutputStream;
 
 public class ServerMain {
     public static void main(String[] args) {
@@ -19,14 +20,32 @@ public class ServerMain {
                 System.out.println("Accepted connection from " + clientSocket);
                 OutputStream outputStream = clientSocket.getOutputStream();
                 outputStream.write(("Hello World \n").getBytes());
-                for(int i=0; i<10; i++) {
-                    outputStream.write(("Time now is " + new Date() + "\n").getBytes());
-                    Thread.sleep(1000);
-                }
-                clientSocket.close();
+                Thread t = new Thread() {
+
+                    public void run() {
+                        try {
+                            handleClientSocket(clientSocket);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                };
+               t.start();
             }
-        } catch (IOException | InterruptedException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
         }
+
+    private static void handleClientSocket(Socket clientSocket) throws IOException, InterruptedException {
+        for(int i=0; i<10; i++) {
+            OutputStream outputStream = clientSocket.getOutputStream();
+            outputStream.write(("Time now is " + new Date() + "\n").getBytes());
+            Thread.sleep(1000);
+        }
+        clientSocket.close();
+    }
 }
