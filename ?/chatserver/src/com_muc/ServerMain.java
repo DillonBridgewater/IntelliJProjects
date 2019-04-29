c9128e2186661c6621839a438dd487f9dc2bc6fe
DillonBridgewater <dbrid3564@westada.org>
2019-04-29 12:22:08 -0600

    initial update First functional telnet + putty

--- ?/chatserver/src/com_muc/ServerMain.java
+++ ?/chatserver/src/com_muc/ServerMain.java
@@ -2,8 +2,10 @@ package com_muc;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.net.DatagramPacket;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Date;
 
 public class ServerMain {
     public static void main(String[] args) {
@@ -16,10 +18,14 @@ public class ServerMain {
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("Accepted connection from " + clientSocket);
                 OutputStream outputStream = clientSocket.getOutputStream();
-                outputStream.write("Hello World\n".getBytes());
+                outputStream.write(("Hello World \n").getBytes());
+                for(int i=0; i<10; i++) {
+                    outputStream.write(("Time now is " + new Date() + "\n").getBytes());
+                    Thread.sleep(1000);
+                }
                 clientSocket.close();
             }
-        } catch (IOException e) {
+        } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
         }
